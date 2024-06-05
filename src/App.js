651aebb7a0afdc75ec711dedb5c350a930f3e784
장정안 <wjddks96@naver.com>
2024-06-05 15:18:27 +0900

    feat: 게임 시작 텍스트 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGES } from "./constants/messages.js";
+
 class App {
-  async play() {}
+  play() {
+    this.printMessage(MESSAGES.START_GAME);
+    this.inputNumber();
+  }
+
+  printMessage(message) {
+    Console.print(message);
+  }
+
+  inputNumber() {
+    Console.readLineAsync(MESSAGES.INPUT_NUMBER);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
