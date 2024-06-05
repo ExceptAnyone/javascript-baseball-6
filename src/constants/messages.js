4a772a9dc5685f1ea58a5e416409be8977542f7d
장정안 <wjddks96@naver.com>
2024-06-05 16:45:53 +0900

    feat: 게임 내 메세지 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,15 @@
 export const MESSAGES = Object.freeze({
   START_GAME: "숫자 야구 게임을 시작합니다.",
   INPUT_NUMBER: "숫자를 입력해주세요 :",
+  NOTHING: "낫싱",
+  GAME_OVER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+});
+
+export const ANSWERS = Object.freeze({
+  ONE_STRIKE: "1스트라이크",
+  TWO_STRIKE: "2스트라이크",
+  THREE_STRIKE: "3스트라이크",
+  ONE_BALL: "1볼",
+  TWO_BALL: "2볼",
+  THREE_BALL: "3볼",
 });
