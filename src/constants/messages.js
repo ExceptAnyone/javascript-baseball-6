4c0016840d5eedd1462bb9d181aa3f3e46d29ca0
장정안 <wjddks96@naver.com>
2024-06-07 16:29:43 +0900

    feat: 에러메세지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,13 +3,25 @@ export const MESSAGES = Object.freeze({
   INPUT_NUMBER: "숫자를 입력해주세요 :",
   NOTHING: "낫싱",
   GAME_OVER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-});
-
-export const ANSWERS = Object.freeze({
   ONE_STRIKE: "1스트라이크",
   TWO_STRIKE: "2스트라이크",
   THREE_STRIKE: "3스트라이크",
   ONE_BALL: "1볼",
   TWO_BALL: "2볼",
   THREE_BALL: "3볼",
+});
+
+export const ANSWERS = Object.freeze({
+  THREE_STRIKE: 3,
+  THREE_BALL: 3,
+  TWO_STRIKE: 2,
+  TWO_BALL: 2,
+  ONE_STRIKE: 1,
+  ONE_BALL: 1,
+});
+
+export const ERROR_MESSAGES = Object.freeze({
+  NOT_NUMBER: "숫자를 입력하세요.",
+  NOT_RANGE: "10 이하의 숫자를 입력하세요.",
+  NOT_LENGTH: "3개의 숫자를 입력해주세요.",
 });
