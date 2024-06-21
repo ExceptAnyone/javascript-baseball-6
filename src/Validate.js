2e3af00221b13c07d494c0e110af4c24473ca5db
장정안 <wjddks96@naver.com>
2024-06-21 21:49:10 +0900

    feat: 유효성 검사 추가

--- src/Validate.js
+++ src/Validate.js
@@ -1,3 +1,4 @@
+import { FLAG } from "./constants/flag.js";
 import { ERROR_MESSAGES } from "./constants/messages.js";
 
 class Validate {
@@ -8,6 +9,21 @@ class Validate {
 
     if (answer.length !== 3) {
       throw new Error(ERROR_MESSAGES.NOT_LENGTH);
+    }
+
+    if (answer.includes("0")) {
+      throw new Error(ERROR_MESSAGES.INCLUDES_ZERO);
+    }
+
+    const set = new Set(answer);
+    if (answer.length !== set.size) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE);
+    }
+  }
+
+  static isRestart(answer) {
+    if (answer !== FLAG.YES && answer !== FLAG.NO) {
+      throw new Error(ERROR_MESSAGES.ISVALID_VALUE);
     }
   }
 }
