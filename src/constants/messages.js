588a20272bbc0700834ed1bc367583c2399927fe
장정안 <wjddks96@naver.com>
2024-06-21 21:49:17 +0900

    feat: 에러 메세지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -3,6 +3,7 @@ export const MESSAGES = Object.freeze({
   INPUT_NUMBER: "숫자를 입력해주세요 :",
   NOTHING: "낫싱",
   GAME_OVER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
   ONE_STRIKE: "1스트라이크",
   TWO_STRIKE: "2스트라이크",
   THREE_STRIKE: "3스트라이크",
@@ -24,4 +25,7 @@ export const ERROR_MESSAGES = Object.freeze({
   NOT_NUMBER: "숫자를 입력하세요.",
   NOT_RANGE: "10 이하의 숫자를 입력하세요.",
   NOT_LENGTH: "3개의 숫자를 입력해주세요.",
+  INCLUDES_ZERO: "0이 포함되어있습니다.",
+  DUPLICATE: "중복된 숫자가 있습니다.",
+  ISVALID_VALUE: "올바른 입력값을 입력해주세요.",
 });
